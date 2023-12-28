b303ff8cef330482329d4593c180752578dd69b2
EvgeniyaDubrovskaya <zenya@tut.by>
2023-12-28 15:43:18 +0400

    Change configuration of window size

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,8 @@ from selene import browser
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://demoqa.com'
-    browser.driver.maximize_window()
+    browser.config.window_width = 1920
+    browser.config.window_height = 1200
 
     yield
 
